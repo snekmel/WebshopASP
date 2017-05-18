9ec5e4ed520db350437be91b72d4f5ba82020b6b
Lars <larslemkens@gmail.com>
2017-05-19 00:53:34 +0200

    -

--- DalWebshop/Models/Order.cs
+++ DalWebshop/Models/Order.cs
@@ -7,9 +7,26 @@ namespace DalWebshop.Models
     {
         public int Id { get; private set; }
         public DateTime Date { get; private set; }
-        public Gebruiker Klant { get; private set; }
-        public Kortingcoupon Kortingcoupon { get; private set; }
-        public List<Product> Producten { get; private set; }
+        public int GebruikerId { get; private set; }
+
+
+        public Kortingcoupon Kortingcoupon { get;  set; }
+        public List<Product> Producten { get;  set; }
+
+
+        public Order(int id, DateTime date, int gebruikerId)
+        {
+            Id = id;
+            Date = date;
+            GebruikerId = gebruikerId;
+            Producten = new List<Product>();
+        }
+        public Order(int gebruikerId)
+        {
+            GebruikerId = gebruikerId;
+            Date = DateTime.Now;
+            Producten = new List<Product>();
+        }
 
     }
 }
---
+++ DalWebshop/Repositorys/DAL/Context/OrderSQLContext.cs
@@ -0,0 +1,96 @@
+﻿using DalWebshop.Models;
+using DalWebshop.Repositorys.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DalWebshop.Repositorys.DAL.Context
+{
+    internal class OrderSQLContext : IMaintanable<Order>
+    {
+        public string Create(Order obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order Retrieve(string key)
+        {
+            Order returnOrder = null;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Env.ConString))
+                {
+                    string query = "Select * from Product where id = @key";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@key", key);
+
+                    string query2 = "Select * from Order_Product where orderId = @orderId";
+                    SqlCommand cmd2 = new SqlCommand(query2, con);
+
+                    con.Open();
+
+                    var reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        returnOrder = new Order(reader.GetInt32(0), reader.GetDateTime(1), reader.GetInt32(2));
+                        if (!reader.IsDBNull(4))
+                        {
+                            KortingcouponSQLContext ksc = new KortingcouponSQLContext();
+                            KortingCouponRepository kcr = new KortingCouponRepository(ksc);
+
+                            returnOrder.Kortingcoupon = kcr.Retrieve("4");
+                        }
+
+                        cmd.Parameters.AddWithValue("@orderId", returnOrder.Id);
+
+                        var reader2 = cmd.ExecuteReader();
+                        while (reader2.Read())
+                        {
+
+                            if (!reader.IsDBNull(1))
+                            {
+
+                                ProductSQLContext psc = new ProductSQLContext();
+                                ProductRepository pr = new ProductRepository(psc);
+
+                                returnOrder.Producten.Add(pr.Retrieve(reader.GetInt32(1).ToString()));
+
+                            }
+
+
+                        }
+                    }
+
+
+
+                
+
+                    con.Close();
+                }
+
+                return returnOrder;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public List<Order> RetrieveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Order obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
